77c0a99146fa20edeb946e61f2af0da676fa1fa5
carvalhocaio <caiocarvalho.py@gmail.com>
2022-11-28 11:11:50 -0400

    creating user objects

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,11 +1,25 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/carvalhocaio/go-crud/src/configuration/rest_err"
+	"github.com/carvalhocaio/go-crud/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	err := rest_err.NewBadRequestError("Você chamou a rota de forma errada!")
-	c.JSON(err.Code, err)
+
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect fields, error=%s", err.Error()),
+		)
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
 }
