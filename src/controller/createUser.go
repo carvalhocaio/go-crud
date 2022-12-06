961737dd4695c647018a6dc75ef058452fa21393
carvalhocaio <caiocarvalho.py@gmail.com>
2022-12-06 00:23:48 -0400

    customizing validation errors

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -2,20 +2,22 @@ package controller
 
 import (
 	"fmt"
+	"log"
 
-	"github.com/carvalhocaio/go-crud/src/configuration/rest_err"
+	"github.com/carvalhocaio/go-crud/src/configuration/validation"
 	"github.com/carvalhocaio/go-crud/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-
+	log.Println("Init CreateUser controller")
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		restErr := rest_err.NewBadRequestError("Some fields are incorrect")
-		fmt.Println(err.Error())
-		c.JSON(restErr.Code, restErr)
+		log.Printf("Error trying to marshal object, error=%s\n", err.Error())
+		errRest := validation.ValidateUserError(err)
+
+		c.JSON(errRest.Code, errRest)
 		return
 	}
 
