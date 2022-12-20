75098a2f2fff9710264aab0a024ed5c12ace700e
carvalhocaio <caiocarvalho.py@gmail.com>
2022-12-20 13:45:37 -0400

    encrypting user's password

---
+++ src/model/user_domain.go
@@ -0,0 +1,36 @@
+package model
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+
+	"github.com/carvalhocaio/go-crud/src/configuration/rest_err"
+)
+
+func NewUserDomain(
+	email, password, name string,
+	age int64,
+) UserDomainInterface {
+	return &UserDomain{email, password, name, age}
+}
+
+type UserDomain struct {
+	Email    string
+	Password string
+	Name     string
+	Age      int64
+}
+
+func (ud *UserDomain) EncryptPassword() {
+	hash := md5.New()
+	defer hash.Reset()
+	hash.Write([]byte(ud.Password))
+	ud.Password = hex.EncodeToString(hash.Sum(nil))
+}
+
+type UserDomainInterface interface {
+	CreateUser() *rest_err.RestErr
+	UpdateUser(string) *rest_err.RestErr
+	FindUser(string) (*UserDomain, *rest_err.RestErr)
+	DeleteUser(string) *rest_err.RestErr
+}
